dc6a5ab8e6ee786bfe1f0c11c866c1f9e9d100c4
Amber Vale <me@ambervale.dev>
2024-12-03 20:56:08 -0500

    Add `<ErrorAlert>` component

---
+++ src/lib/components/ui/alert/ErrorAlert.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { AlertOctagonIcon } from "lucide-svelte";
+  import type { Snippet } from "svelte";
+
+  interface AlertProps {
+    children: Snippet;
+  }
+
+  const { children }: AlertProps = $props();
+</script>
+
+<div
+  class="p-3 text-sm text-destructive dark:text-destructive-foreground items-center bg-destructive/10 dark:bg-destructive/50 rounded-md text-center flex flex-row gap-x-2"
+>
+  <AlertOctagonIcon size={22} />
+  {@render children()}
+</div>
