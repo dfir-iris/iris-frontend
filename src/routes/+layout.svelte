85a44802007523db07dcaa48d77e90f6b41639d6
Amber Vale <me@ambervale.dev>
2024-12-04 21:41:10 -0500

    [IMP] Fix broken ModeWatcher in sveltev5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,26 @@
 <script lang="ts">
   import { authUserStore } from "$lib/stores/auth.store";
   import "../app.css";
-  import { ModeWatcher } from "mode-watcher";
+  import { ModeWatcher, mode } from "mode-watcher";
 
   const { children, data } = $props();
 
   $effect.pre(() => {
+    // Set user state into store
     authUserStore.set(data.user);
     console.debug("UserInfo", data.user);
+
+    // Set UI theme
+    if ($mode == "dark") {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
   });
 </script>
 
 <!-- Light/dark scheme monitor -->
-<ModeWatcher />
+<ModeWatcher track={false} />
 
 <!-- Render subroutes -->
 {@render children()}
