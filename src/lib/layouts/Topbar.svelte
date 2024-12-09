d41174a1422a31a2f452b76711f4bce07e2f344c
Amber Vale <me@ambervale.dev>
2024-12-09 12:51:28 -0500

    [IMP] Ensure topbar has fixed height; prettier formatting

--- src/lib/layouts/Topbar.svelte
+++ src/lib/layouts/Topbar.svelte
@@ -1,124 +1,105 @@
 <script>
-  import {
-  BellIcon,
-    ClipboardList,
-    LogOutIcon,
-    MoonIcon,
-    SearchIcon,
-    SettingsIcon,
-    SlidersHorizontalIcon,
-    SunIcon,
-    UserRoundIcon,
-  } from "lucide-svelte";
-  import Button from "$lib/components/ui/button/button.svelte";
-  import Input from "$lib/components/ui/input/input.svelte";
-  import {
-    DropdownMenu,
-    DropdownMenuLabel,
-    DropdownMenuContent,
-    DropdownMenuTrigger,
-    DropdownMenuItem,
-    DropdownMenuSeparator,
-  } from "$lib/components/ui/dropdown-menu";
-  import { authUserStore } from "$lib/stores/auth.store";
-  import { mode, toggleMode } from "mode-watcher";
+	import {
+		BellIcon,
+		ClipboardList,
+		LogOutIcon,
+		MoonIcon,
+		SearchIcon,
+		SettingsIcon,
+		SlidersHorizontalIcon,
+		SunIcon,
+		UserRoundIcon
+	} from 'lucide-svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Input from '$lib/components/ui/input/input.svelte';
+	import {
+		DropdownMenu,
+		DropdownMenuLabel,
+		DropdownMenuContent,
+		DropdownMenuTrigger,
+		DropdownMenuItem,
+		DropdownMenuSeparator
+	} from '$lib/components/ui/dropdown-menu';
+	import { authUserStore } from '$lib/stores/auth.store';
+	import { mode, toggleMode } from 'mode-watcher';
 </script>
 
 <header
-  class="sticky top-0 h-16 bg-primary-gradient flex flex-row items-center px-4 shadow"
+	class="sticky top-0 flex h-16 shrink-0 flex-row items-center bg-primary-gradient px-4 shadow"
 >
-  <!-- Logo -->
-  <a href="/">
-    <img
-      src="/logo/logo-white.png"
-      alt="IRIS Logo"
-      class="transition-all duration-300 w-[100px]"
-    />
-  </a>
+	<!-- Logo -->
+	<a href="/">
+		<img src="/logo/logo-white.png" alt="IRIS Logo" class="w-[100px] transition-all duration-300" />
+	</a>
 
-  <div class="ml-auto"></div>
+	<div class="ml-auto"></div>
 
-  <!-- Search bar -->
-  <div class="relative group pr-2">
-    <Input type="text" placeholder="Search" class="bg-background pl-8"></Input>
-    <SearchIcon
-      size={20}
-      class="absolute top-2 left-2 opacity-70 group-focus-within:opacity-100"
-    />
-  </div>
+	<!-- Search bar -->
+	<div class="group relative pr-2">
+		<Input type="text" placeholder="Search" class="bg-background pl-8"></Input>
+		<SearchIcon size={20} class="absolute left-2 top-2 opacity-70 group-focus-within:opacity-100" />
+	</div>
 
-  <!-- Tasks dropdown -->
-  <DropdownMenu>
-    <DropdownMenuTrigger>
-      <Button variant="ghost" class="px-2 text-gray-100"
-        ><ClipboardList /></Button
-      >
-    </DropdownMenuTrigger>
-    <DropdownMenuContent class="w-56">
-      <DropdownMenuLabel>
-        Tasks
-      </DropdownMenuLabel>
+	<!-- Tasks dropdown -->
+	<DropdownMenu>
+		<DropdownMenuTrigger>
+			<Button variant="ghost" class="px-2 text-gray-100"><ClipboardList /></Button>
+		</DropdownMenuTrigger>
+		<DropdownMenuContent class="w-56">
+			<DropdownMenuLabel>Tasks</DropdownMenuLabel>
 
-      <DropdownMenuSeparator />
-      <DropdownMenuItem href="/tasks">
-        <span>View all</span>
-      </DropdownMenuItem>
-    </DropdownMenuContent>
-  </DropdownMenu>
+			<DropdownMenuSeparator />
+			<DropdownMenuItem href="/tasks">
+				<span>View all</span>
+			</DropdownMenuItem>
+		</DropdownMenuContent>
+	</DropdownMenu>
 
-  <!-- Alerts dropdown -->
-  <DropdownMenu>
-    <DropdownMenuTrigger>
-      <Button variant="ghost" class="px-2 text-gray-100"
-        ><BellIcon /></Button
-      >
-    </DropdownMenuTrigger>
-    <DropdownMenuContent class="w-56">
-      <DropdownMenuLabel>
-        Alerts
-      </DropdownMenuLabel>
+	<!-- Alerts dropdown -->
+	<DropdownMenu>
+		<DropdownMenuTrigger>
+			<Button variant="ghost" class="px-2 text-gray-100"><BellIcon /></Button>
+		</DropdownMenuTrigger>
+		<DropdownMenuContent class="w-56">
+			<DropdownMenuLabel>Alerts</DropdownMenuLabel>
 
-      <DropdownMenuSeparator />
-      <DropdownMenuItem href="/alerts">
-        <span>View all</span>
-      </DropdownMenuItem>
-    </DropdownMenuContent>
-  </DropdownMenu>
+			<DropdownMenuSeparator />
+			<DropdownMenuItem href="/alerts">
+				<span>View all</span>
+			</DropdownMenuItem>
+		</DropdownMenuContent>
+	</DropdownMenu>
 
-  <!-- User dropdown -->
-  <DropdownMenu>
-    <DropdownMenuTrigger>
-      <Button variant="ghost" class="px-2 text-gray-100"
-        ><UserRoundIcon /></Button
-      >
-    </DropdownMenuTrigger>
-    <DropdownMenuContent class="w-56">
-      <DropdownMenuLabel
-        >{$authUserStore?.name || "My account"}</DropdownMenuLabel
-      >
-      <DropdownMenuItem >
-        <SlidersHorizontalIcon size={18} />
-        <span>Preferences</span>
-      </DropdownMenuItem>
-      <DropdownMenuItem on:click={toggleMode}>
-        {#if $mode == "light"}
-          <MoonIcon size={18} />
-          <span>Dark mode</span>
-        {:else}
-          <SunIcon size={18} />
-          <span>Light mode</span>
-        {/if}
-      </DropdownMenuItem>
-      <DropdownMenuSeparator />
-      <DropdownMenuItem>
-        <SettingsIcon size={18} />
-        <span>Manage IRIS</span>
-      </DropdownMenuItem>
-      <DropdownMenuSeparator />
-      <DropdownMenuItem>
-        <LogOutIcon size={18} />
-        <span>Log out</span>
-      </DropdownMenuItem>
-    </DropdownMenuContent>
-  </DropdownMenu>
+	<!-- User dropdown -->
+	<DropdownMenu>
+		<DropdownMenuTrigger>
+			<Button variant="ghost" class="px-2 text-gray-100"><UserRoundIcon /></Button>
+		</DropdownMenuTrigger>
+		<DropdownMenuContent class="w-56">
+			<DropdownMenuLabel>{$authUserStore?.name || 'My account'}</DropdownMenuLabel>
+			<DropdownMenuItem>
+				<SlidersHorizontalIcon size={18} />
+				<span>Preferences</span>
+			</DropdownMenuItem>
+			<DropdownMenuItem on:click={toggleMode}>
+				{#if $mode == 'light'}
+					<MoonIcon size={18} />
+					<span>Dark mode</span>
+				{:else}
+					<SunIcon size={18} />
+					<span>Light mode</span>
+				{/if}
+			</DropdownMenuItem>
+			<DropdownMenuSeparator />
+			<DropdownMenuItem>
+				<SettingsIcon size={18} />
+				<span>Manage IRIS</span>
+			</DropdownMenuItem>
+			<DropdownMenuSeparator />
+			<DropdownMenuItem>
+				<LogOutIcon size={18} />
+				<span>Log out</span>
+			</DropdownMenuItem>
+		</DropdownMenuContent>
+	</DropdownMenu>
 </header>
