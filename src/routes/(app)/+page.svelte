16b54aac8d98423c871f0309b4a901383f886279
Amber Vale <me@ambervale.dev>
2024-12-22 17:21:24 -0500

    [IMP] Update dashboard header

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -85,7 +85,7 @@
 
 <div class="space-y-4 p-8 pt-6">
 	<div class="flex items-center justify-between space-y-2">
-		<h2 class="text-3xl font-bold tracking-tight">Dashboard</h2>
+		<h1 class="">Dashboard</h1>
 		<div class="flex items-center space-x-2"></div>
 	</div>
 	<Tabs.Root value={activeTab} class="space-y-4" on:change={(e) => handleTabChange(e.detail.value)}>
