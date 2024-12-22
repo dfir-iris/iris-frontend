51633ea60955bffb2dd9a324b9e8d67ff8b16b3e
Amber Vale <me@ambervale.dev>
2024-12-22 18:41:09 -0500

    [ADD] Basic case views

--- src/routes/(app)/case/[case_id]/+page.svelte
+++ src/routes/(app)/case/[case_id]/+page.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
-  import type { PageData } from "./$types";
+	import { Badge } from '$lib/components/ui/badge';
+	import SeverityBadge from '$lib/components/ui/badge/severity-badge.svelte';
+	import StatusBadge from '$lib/components/ui/badge/status-badge.svelte';
+	import { TagIcon } from 'lucide-svelte';
+	import type { PageData } from './$types';
 
-  let { data }: { data: PageData } = $props();
+	let { data }: { data: PageData } = $props();
+
+	$inspect(data);
 </script>
 
-<h1>content</h1>
+<!-- Overview info -->
+<div class="flex w-full flex-col border-b bg-muted/20 p-4">
+	{#await data.case}
+		loader
+	{:then { data: caseData }}
+		<p>Customer #{caseData.case_customer}</p>
+		<h1>{caseData.case_name}</h1>
+		<div class="flex flex-row gap-x-2 py-2">
+			<StatusBadge status="In progress"></StatusBadge>
+			<SeverityBadge severity="High"></SeverityBadge>
+			<Badge icon={TagIcon}>tag1</Badge>
+			<Badge icon={TagIcon}>tag2</Badge>
+			<Badge icon={TagIcon}>tag3</Badge>
+		</div>
+	{/await}
+</div>
--- src/routes/(app)/case/[case_id]/overview/+page.svelte
+++ src/routes/(app)/case/[case_id]/overview/+page.svelte
@@ -3,3 +3,5 @@
 
 	let { data }: { data: PageData } = $props();
 </script>
+
+<h1>Overview</h1>
