aba62c01b1d8d5e416f83c4994f9960b234630cd
Amber Vale <me@ambervale.dev>
2024-12-25 15:10:12 -0500

    [ADD] Searchbar to Cases UI

--- src/routes/(app)/cases/+page.svelte
+++ src/routes/(app)/cases/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import CasesDataTable from '$lib/components/common/cases-data-table.svelte';
+	import Searchbar from '$lib/components/ui/searchbar/searchbar.svelte';
 	import type { PageData } from './$types';
 
 	let { data }: { data: PageData } = $props();
@@ -10,7 +12,13 @@
 </svelte:head>
 
 <div class="flex h-full flex-col gap-y-6 p-8">
-	<h1>Cases</h1>
+	<div class="flex flex-row items-center">
+		<h1>Cases</h1>
+
+		<div class="ml-auto"></div>
+		<Searchbar placeholder="Search cases" bind:value={page.url.searchParams.search}></Searchbar>
+		<!-- FIXME: need to add a store that maps to URL params -->
+	</div>
 
 	<CasesDataTable cases={data.cases}></CasesDataTable>
 </div>
