8f55d8a951dfac1f595a34adc94873341855dcdf
Amber Vale <me@ambervale.dev>
2024-12-25 15:07:28 -0500

    [ADD] Searchbar component

---
+++ src/lib/components/ui/searchbar/searchbar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { SearchIcon } from 'lucide-svelte';
+	import { Input } from '../input';
+
+	let {
+		value = $bindable(),
+		placeholder = 'Search'
+	}: {
+		value: string;
+		placeholder?: string;
+	} = $props();
+</script>
+
+<div class="group relative pr-2">
+	<Input bind:value type="text" {placeholder} class="bg-background pl-8"></Input>
+	<SearchIcon class="absolute left-2 top-2 !h-5 !w-5 opacity-70 group-focus-within:opacity-100" />
+</div>
