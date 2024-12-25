50fda8facc8ca64d726cb176cdc6b7b73062722d
Amber Vale <me@ambervale.dev>
2024-12-25 15:10:00 -0500

    [IMP] Remove searchbar right padding

--- src/lib/components/ui/searchbar/searchbar.svelte
+++ src/lib/components/ui/searchbar/searchbar.svelte
@@ -11,7 +11,7 @@
 	} = $props();
 </script>
 
-<div class="group relative pr-2">
+<div class="group relative">
 	<Input bind:value type="text" {placeholder} class="bg-background pl-8"></Input>
 	<SearchIcon class="absolute left-2 top-2 !h-5 !w-5 opacity-70 group-focus-within:opacity-100" />
 </div>
