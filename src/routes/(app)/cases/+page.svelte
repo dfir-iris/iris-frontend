6e4501bd228346585c1d15712adeeaaab89ee660
Amber Vale <me@ambervale.dev>
2024-12-25 14:19:56 -0500

    [ADD] Basic cases endpoint

---
+++ src/routes/(app)/cases/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import CasesDataTable from '$lib/components/common/cases-data-table.svelte';
+	import type { PageData } from './$types';
+
+	let { data }: { data: PageData } = $props();
+</script>
+
+<svelte:head>
+	<title>Cases | DFIR-IRIS</title>
+</svelte:head>
+
+<div class="flex h-full flex-col gap-y-6 p-8">
+	<h1>Cases</h1>
+
+	<CasesDataTable cases={data.cases}></CasesDataTable>
+</div>
---
+++ src/routes/(app)/cases/+page.ts
@@ -0,0 +1,10 @@
+import { ENDPOINTS } from '$lib/constants/endpoints';
+import { ApiService, type Paginated } from '$lib/services/api.service';
+import type { Case } from '$lib/types/resources/case';
+import type { PageLoad } from './$types';
+
+export const load = (async ({ fetch }) => {
+  return {
+    cases: ApiService.get<Paginated<Case>>(`${ENDPOINTS.case.list}`, {}, fetch),
+  };
+}) satisfies PageLoad;
