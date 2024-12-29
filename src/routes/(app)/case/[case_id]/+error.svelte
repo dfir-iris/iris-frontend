6554552d37780d73ebebc6f37431f24654073980
Amber Vale <me@ambervale.dev>
2024-12-29 10:21:27 -0500

    [IMP] Make error page look pretty

--- src/routes/(app)/case/[case_id]/+error.svelte
+++ src/routes/(app)/case/[case_id]/+error.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
+	import { ExternalLinkIcon, TrafficConeIcon } from 'lucide-svelte';
 </script>
 
-<h1>{$page.status}: {$page.error?.message}</h1>
+<div class="flex h-full w-full flex-col items-center justify-center gap-y-2">
+	<TrafficConeIcon size={72} class="text-blue-800 dark:text-blue-600" />
+	<h1>Error {page.status}</h1>
+	<h2>{page.error?.message}</h2>
+	<p>Our apologies.</p>
+	<!-- TODO: ADD LINK -->
+	<a href="" class="flex flex-row items-center gap-x-1 underline"
+		>Report an issue on our GitHub <ExternalLinkIcon size={16} /></a
+	>
+</div>
