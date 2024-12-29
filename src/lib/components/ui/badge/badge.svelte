a6be5af9bc0426e60efd08fff6cf2c79c633c598
Amber Vale <me@ambervale.dev>
2024-12-29 16:01:32 -0500

    [ADD] Tooltip option to the badge

--- src/lib/components/ui/badge/badge.svelte
+++ src/lib/components/ui/badge/badge.svelte
@@ -2,6 +2,8 @@
 	import { type Variant, badgeVariants } from './index.js';
 	import { cn } from '$lib/utils.js';
 	import type { Icon } from 'lucide-svelte';
+	import { Tooltip, TooltipTrigger } from '../tooltip/index.js';
+	import TooltipContent from '../tooltip/tooltip-content.svelte';
 
 	let className: string | undefined | null = undefined;
 	export let href: string | undefined = undefined;
@@ -9,14 +11,20 @@
 	export { className as class };
 
 	export let icon: typeof Icon | undefined = undefined;
+	export let tooltip: string | undefined = undefined;
 </script>
 
-<svelte:element
-	this={href ? 'a' : 'span'}
-	{href}
-	class={cn(badgeVariants({ variant, className }), 'gap-x-1')}
-	{...$$restProps}
->
-	<svelte:component this={icon} class="!h-3 !w-3" />
-	<slot />
-</svelte:element>
+<Tooltip>
+	<TooltipTrigger class="cursor-default">
+		<svelte:element
+			this={href ? 'a' : 'span'}
+			{href}
+			class={cn(badgeVariants({ variant, className }), 'gap-x-1')}
+			{...$$restProps}
+		>
+			<svelte:component this={icon} class="h-3.5 w-3.5" />
+			<slot />
+		</svelte:element>
+	</TooltipTrigger>
+	<TooltipContent>{tooltip}</TooltipContent>
+</Tooltip>
