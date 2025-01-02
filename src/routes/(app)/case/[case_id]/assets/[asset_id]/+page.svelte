5c4c2acd6b2d959e88e21064059e85318a62f1ca
Amber Vale <me@ambervale.dev>
2025-01-02 16:24:47 -0500

    [IMP] stub out asset details view a bit

--- src/routes/(app)/case/[case_id]/assets/[asset_id]/+page.svelte
+++ src/routes/(app)/case/[case_id]/assets/[asset_id]/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+	import { Tabs, TabsList, TabsTrigger } from '$lib/components/ui/tabs';
 	import type { PageData } from './$types';
 
 	let { data }: { data: PageData } = $props();
 </script>
+
+<h1 class="mb-4">pc2</h1>
+<Tabs>
+	<TabsList class="border">
+		<TabsTrigger value="details">Details</TabsTrigger>
+		<TabsTrigger value="graph">Graph</TabsTrigger>
+	</TabsList>
+</Tabs>
--- src/routes/(app)/case/[case_id]/assets/[asset_id]/+page.ts
+++ src/routes/(app)/case/[case_id]/assets/[asset_id]/+page.ts
@@ -1,9 +1,10 @@
 import { ENDPOINTS } from '$lib/constants/endpoints';
 import { ApiService } from '$lib/services/api.service';
+import type { Asset } from '$lib/types/resources/asset';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ params }) => {
   return {
-    asset: ApiService.get(ENDPOINTS.case.assets.getById(params.asset_id))
+    asset: ApiService.get<Asset>(ENDPOINTS.case.assets.getById(params.asset_id))
   };
 }) satisfies PageLoad;
