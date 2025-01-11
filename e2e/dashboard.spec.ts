eaab376385f0d1bfd1ee21e3bfbb783db70e43b1
Amber Vale <me@ambervale.dev>
2025-01-10 19:45:01 -0500

    [IMP] Enhance dashboard tests to test toggling and links

--- e2e/dashboard.spec.ts
+++ e2e/dashboard.spec.ts
@@ -2,9 +2,23 @@ import { test, expect } from '@playwright/test';
 
 test('test dashboard populates', async ({ page }) => {
   await page.goto('http://localhost:4173');
+  await expect(page.getByRole('banner')).toBeVisible();
   await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+
+  // default to seeing owned cases
   await expect(page.getByRole('heading', { name: 'Owned Cases' })).toBeVisible();
+  await expect(page).toHaveURL(/#cases/);
   await expect(page.locator('.p-6 > .flex').first()).toBeVisible();
+
+  // hide the owned cases by clicking
+  await page.locator('.p-6').first().click();
+  await expect(page).toHaveURL(/#/);
+  await expect(page.getByRole('heading', { name: 'Owned Cases' })).not.toBeVisible();
+
+  // show owned cases by clicking again
+  await page.locator('.p-6').first().click();
+  await expect(page).toHaveURL(/#cases/);
+  await expect(page.getByRole('heading', { name: 'Owned Cases' })).toBeVisible();
   await expect(page.getByRole('link', { name: '#1 - Initial Demo' })).toBeVisible();
   await expect(page.getByRole('link', { name: '#2 - Initial Demo' })).toBeVisible();
 });
