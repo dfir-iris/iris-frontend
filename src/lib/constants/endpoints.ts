7f4d98ac7e480e881e659b1b34b5231a771c45a4
Amber Vale <me@ambervale.dev>
2025-01-26 12:22:23 -0500

    [ADD] buildUrl helper for endpoints

--- src/lib/constants/endpoints.ts
+++ src/lib/constants/endpoints.ts
@@ -1,3 +1,18 @@
+/** Builds and formats an API endpoint with URL params. */
+const buildUrl = (endpoint: string, params: Record<string, string | string[] | number | number[] | boolean>) => {
+    for (const [k, v] of Object(params).entries()) {
+        // Convert arrays to string separated with commas
+        if (Array.isArray(v)) {
+            params[k] = v.join(',')
+            continue
+        }
+        // Let everything else get coerced to string
+        params[k] = `${v}`
+    }
+    const urlParams = new URLSearchParams(params as Record<string, string>)
+    return `${endpoint}?${urlParams.toString()}`
+}
+
 export const ENDPOINTS = {
     dashboard: {
         cases: {
